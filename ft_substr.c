b6d5789dcc106a2976fbdb15e61193fd4b5ef3c4
Thea Lee <thelee@bess-f1r1s14.clusters.42paris.fr>
2024-08-06 19:35:01 +0200

    rendu

---
+++ ft_substr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thelee <thelee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 19:17:11 by thelee            #+#    #+#             */
+/*   Updated: 2024/06/15 15:19:31 by thelee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if ((size_t)start > ft_strlen(s))
+		return (ft_strdup("\0"));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+z
